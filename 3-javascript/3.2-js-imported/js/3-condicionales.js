3a85be12a4fa0fe908d0d6e5f23ae71cc7cffeec
manuel_859 <manuel_859@outlook.es>
2023-04-26 19:56:36 -0600

    mi primer commit

---
+++ 3-javascript/3.2-js-imported/js/3-condicionales.js
@@ -0,0 +1,57 @@
+/**
+ * condicionales son instrucciones con condición cuando un evento debe de pasar
+*/
+
+/* estructura del if
+if(condicion){
+    console.log("extraer video chamspion")
+}
+
+
+----------------------------
+estructura del else
+} */
+
+
+/*
+var video= '';
+var clickInMenu=false;
+
+if(video == 'Champion'){
+console.log("ver video champion");
+} else if(video == 'MIRANDA'){
+    console.log("ver video miranda");
+}
+
+if(video == 'MIRANDA'){
+    console.log("ver video Miranda");
+}
+
+if(clickInMenu==true){
+    console.log("mostrando menu..");
+}
+
+var isOldMayor =0;
+
+if(isOldMayor == 1){
+    console.log('Puedo usar esta pagina web')
+}else{
+    console.log('mentiroso')
+}*/
+
+
+var estadoLampara;
+var estadoFoco;
+
+
+if(estadoLampara == 'enchufado'){
+    if(estadoFoco == 'quemado'){
+        console.log("Remplazar el foco")
+    }else{
+        console.log("Comprar nueva lampara")
+    }
+}else{
+    console.log('enchufarla');
+
+
+}
---
+++ 5-manipulacion-dom/JS/5.1-Manipulacion.js
@@ -0,0 +1,5 @@
+//Cambio de contenido
+
+document.getElementById('title').innerHTML = 'Ya no soy tan cruel';
+
+document.getElementById('botonEjemplo').style.backgroundColor = 'red';
